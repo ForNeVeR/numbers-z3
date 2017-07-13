f01e7cdf237ec8f1ba086f73d0f0144f80c83b45
Friedrich von Never <friedrich@fornever.me>
2017-07-13 22:46:28 +0700

    Program: refactor for generic solutions

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -7,50 +7,43 @@ namespace ConsoleApp4
 {
     static class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             using (var context = new Context())
             {
-                var @operator = context.MkEnumSort("operator", "Plus", "Minus", "Mult", "Div");
-                var plus = @operator.Consts[0];
-                var minus = @operator.Consts[1];
-                var mult = @operator.Consts[2];
+                const int minNumber = 1;
+                const int numberCount = 6;
+                const int result = 35;
 
-                IntExpr myf(IntExpr x, Expr z, IntExpr y) =>
-                    (IntExpr)context.MkITE(context.MkEq(z, plus), context.MkAdd(x, y),
-                        context.MkITE(context.MkEq(z, minus), context.MkSub(x, y),
-                            context.MkITE(context.MkEq(z, mult), context.MkMul(x, y),
+
+                var operatorType = context.MkEnumSort("operator", "Plus", "Minus", "Mult", "Div");
+                var plus = operatorType.Consts[0];
+                var minus = operatorType.Consts[1];
+                var mult = operatorType.Consts[2];
+
+                var operands = Enumerable.Range(minNumber, numberCount)
+                    .Select(i => context.MkInt(i))
+                    .ToList();
+                var operators = Enumerable.Range(minNumber, numberCount - 1)
+                    .Select(i => context.MkConst($"op{i}", operatorType))
+                    .ToList();
+
+                IntExpr applyOperator(IntExpr x, Expr @operator, IntExpr y) =>
+                    (IntExpr)context.MkITE(context.MkEq(@operator, plus), context.MkAdd(x, y),
+                        context.MkITE(context.MkEq(@operator, minus), context.MkSub(x, y),
+                            context.MkITE(context.MkEq(@operator, mult), context.MkMul(x, y),
                                 context.MkDiv(x, y))));
 
                 var solver = context.MkSolver();
 
-                var b1 = context.MkConst("b1", @operator);
-                var b2 = context.MkConst("b2", @operator);
-                var b3 = context.MkConst("b3", @operator);
-                var b4 = context.MkConst("b4", @operator);
-                var b5 = context.MkConst("b5", @operator);
+                var equation = applyOperator(operands[0], operators[0], operands[1]);
+                for (var i = 2; i < operands.Count; ++i)
+                {
+                    equation = applyOperator(equation, operators[i - 1], operands[i]);
+                }
 
-                solver.Assert(
-                    context.MkEq(
-                        myf(
-                            myf(
-                                myf(
-                                    myf(
-                                        myf(
-                                            context.MkInt(1),
-                                            b1,
-                                            context.MkInt(2)),
-                                        b2,
-                                        context.MkInt(3)),
-                                    b3,
-                                    context.MkInt(4)),
-                                b4,
-                                context.MkInt(5)),
-                            b5,
-                            context.MkInt(6)),
-                        context.MkInt(35)));
+                solver.Assert(context.MkEq(equation, context.MkInt(result)));
 
-                var operators = new[] { b1, b2, b3, b4, b5 };
                 var solutions = new List<Expr[]>();
                 var step = 1;
                 while (solver.Check() == Status.SATISFIABLE)
